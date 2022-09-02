7f8e002427eefad6af8799241941fbe2af3b6c45
JongWon <3338761@naver.com>
2022-09-03 00:56:58 +0900

    Initial commit

---
+++ src/main/java/com/example/study_sb/StudySbApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_sb/StudySbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
